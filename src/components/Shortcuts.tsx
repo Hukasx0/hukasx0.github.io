f083f01af6a5845802132dcba724a7cc15e6028c
Hukasx0 <hubertkasp13@gmail.com>
2024-01-07 13:27:29 +0100

    Add icon that redirects to the blog

--- src/components/Shortcuts.tsx
+++ src/components/Shortcuts.tsx
@@ -1,4 +1,4 @@
-import {  Drvspace7, Packager, Wordpad, Computer, Shell3233, Mspaint, Shell3241 } from '@react95/icons'
+import {  Drvspace7, Packager, Wordpad, Computer, Bookmark, Mspaint, Shell3241 } from '@react95/icons'
 import styled, { css } from 'styled-components';
 
 interface IconProps {
@@ -29,11 +29,20 @@ function Shortcuts(openAbout: (event: React.MouseEvent<Element, MouseEvent>) => 
                    openPortfolio: (event: React.MouseEvent<Element, MouseEvent>) => void,
                    openSkills: (event: React.MouseEvent<Element, MouseEvent>) => void,
                    openWebamp: (event: React.MouseEvent<Element, MouseEvent>) => void) {
+
+  const redirect = (href: string) => () => {
+    window.open(href, '_blank');
+  };
+  
   return (
     <>
     <Icon>
-    <Computer variant="32x32_4"/>
+      <Computer variant="32x32_4"/>
         My computer
+    </Icon>
+    <Icon clickable={true}>
+      <Bookmark variant="32x32_4" onClick={redirect("https://hukasx0.github.io/blog")}/>
+        Blog.html
     </Icon>
     <Icon clickable={true}>
       <Shell3241 variant="32x32_4" onClick={openWebamp}/>
@@ -54,10 +63,6 @@ function Shortcuts(openAbout: (event: React.MouseEvent<Element, MouseEvent>) => 
     <Icon clickable={true}>
       <Mspaint variant="32x32_4" onClick={openSkills}/>
         Skills
-    </Icon>
-    <Icon>
-      <Shell3233 variant="32x32_4" />
-        Trash
     </Icon>
     </>
   )
